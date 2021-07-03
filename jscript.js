c614122ec1aace911f650bcf3087665c29629178
Erin Lim <erinlim@utexas.edu>
2021-07-03 02:14:25 -0500

    added way to compare current time to time in html

--- jscript.js
+++ jscript.js
@@ -1,4 +1,28 @@
 var today = moment();
+var jumboDate = $("#currentDay");
+var timeBlock = $(".time-block");
 
-$("#currentDay").text(today.format("dddd, MMMM Do"));
+jumboDate.text(today.format("dddd, MMMM Do"));
 
+function checkTime() 
+{
+    timeBlock.each(function ()
+    {   
+        //turns id string for each row div into number
+        var timeID = parseInt(timeBlock.attr('id'));
+        var currentHour = moment().format('h');
+        var rowColor = timeBlock.children('.description');
+
+        if (timeID == currentHour) {
+            rowColor.addClass('present');
+        } else if (timeID > currentHour) {
+            rowColor.addClass('future');
+        } else {
+            rowColor.addClass('past');
+        }
+    })
+}
+
+
+
+checkTime();
